47048fd868f5eb14dec0f741f975c0044f7d2ca5
Angus Lowe <anguslowe@live.ca>
2016-05-07 23:58:04 +0100

    initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/test/java/ServerRule.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ServerRule {
+    public static void main(String args[]) {
+
+    }
+}
